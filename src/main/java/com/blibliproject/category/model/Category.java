01bb99f80fee32db5db1580043c7359be4527ea5
WendyYanto <yanto.wendy98@gmail.com>
2019-03-04 16:05:34 +0700

    Initial Commit

---
+++ src/main/java/com/blibliproject/category/model/Category.java
@@ -0,0 +1,32 @@
+package com.blibliproject.category.model;
+
+public class Category {
+
+    private String name;
+    private String created_at;
+    private String updated_at;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+}
